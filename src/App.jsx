87f19f4d6929d93c6ea31608ef74a5ccee7e91e7
Joseph <indiekaj@gmail.com>
2024-06-30 11:41:59 +0300

    validating props

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import { UserProfile } from './component/userProfile'
 
 export default function App(){
+    const callMe = () => {
+        console.log("Hello");
+        <h2>Hello functions</h2>
+    }
     return <div>
         <h1> New component</h1>
-        <UserProfile age={13} />
-
+        <UserProfile 
+        age={13} username="Joseph I" callMe = {callMe}
+        favoriteFoods = {[
+            {
+                name: "Ugali",
+                id: "qwer"
+            },
+            {
+                name: "Samaki",
+                id: "smk"
+            }
+        ]}
+        />
+        
     </div>
 }
--- src/component/userProfile.jsx
+++ src/component/userProfile.jsx
@@ -1,11 +1,13 @@
-/* eslint-disable react/prop-types */
+import PropTypes from 'prop-types';
 import { Contact } from './contact';
-import { UserFavFoods} from './userFavoriteFoods'
-import { UserUsername } from './UserUsername'
+import { UserFavFoods} from './userFavoriteFoods';
+import { UserUsername } from './UserUsername';
+
 
 export function UserProfile(props){
     var age = props.age + 12;
-    const username = "Joseph I";
+    props.callMe()
+    const username = props.username;
     return <div id="user-profile">
         <UserUsername username={username} />
         <b>Age:</b>
@@ -14,7 +16,20 @@ export function UserProfile(props){
             <span>Email: </span>
             <span>Indieka@gm.com</span>
         </div>
+        {/* <section>
+            <p>{props.favoriteFoods}</p>
+        </section> */}
         <Contact username = {username} code = {500000} />
         <UserFavFoods />
     </div>
+}
+
+UserProfile.propTypes = {
+    username: PropTypes.string.isRequired,
+    age: PropTypes.number,
+    callMe: PropTypes.func.isRequired,
+    favoriteFoods: PropTypes.arrayOf(PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        id: PropTypes.string.isRequired
+    }))
 }
