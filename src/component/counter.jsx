2a8dfca5c7eb894ed29ab49e3f7f842f92f8be54
Joseph <indiekaj@gmail.com>
2024-07-04 22:42:34 +0300

    useEffect and GET request

--- src/component/counter.jsx
+++ src/component/counter.jsx
@@ -6,7 +6,39 @@ export function Counter() {
     useEffect(()=> {
         document.title = "Joseph Learning React" + counter
     }, [counter]);
+
+    useEffect(()=>{
+        const controller = new AbortController ();
+        /*fetch('https://jsonplaceholder.typicode.com/users', {
+            method: 'GET'
+        }).then((res)=>{
+            return res.json()
+        }).then((data) => {
+            console.log(data);
+        }).catch((e) => {
+            console.log(e);
+            console.log("Invalid endpoints");
+        })*/
+       async function fetchUser (){
+        try {
+            const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+                signal: controller.signal
+            });
+            const data = await res.json();
+            console.log(data);
+        } catch (error) {
+            console.log(error);
+        }
+        
+       }
+       fetchUser();
+       return () => {
+        controller.abort();
+       }
+    },[])
+   
     return (
+        
         <div>
             <h1><span>Counter is: {counter}</span></h1>
             <button type="button" onClick={() => setCounter((count) => count + 1)}>Increase</button>
