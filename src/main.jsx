31ac1de5e1569c2abca3af94902148c31c30eeb3
Joseph <indiekaj@gmail.com>
2024-07-08 01:43:12 +0300

    routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './global.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './global.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { RegisterForm } from './component/RegisterForm';
+import { UsersPage } from './pages/UsersFile';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  },
+  {
+    path: '/register',
+    element: <RegisterForm/>
+  },
+  {
+    path: '/users',
+    element: <UsersPage/>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
---
+++ src/pages/UsersFile.jsx
@@ -0,0 +1,8 @@
+export function UsersPage() {
+    return (
+        <div>
+            <h1>Welcome to the Users page</h1>
+        </div>
+    );
+    
+}
